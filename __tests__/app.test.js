0ea2898be5b5e9dfe3b2b963ba7dddb395b58627
IanTibble1 <iantibble1@gmail.com>
2023-08-14 14:16:38 +0100

    adds /api/ feature and testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const db = require("../db/connection");
 const request = require("supertest");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
+const apiList = require("../endpoints.json");
 
 afterAll(() => {
   return db.end();
@@ -18,7 +19,22 @@ describe("app()", () => {
       return request(app).get("/api/topics").expect(200);
     });
 
-    test("GET 404: /api/topics returns a 404 message when there is an error in request ", () => {
+    test("GET 200: /api/topics should return an array of topic objects which has the slug and description properties ", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then((data) => {
+          const { body } = data;
+          const topics = body.topics;
+          topics.forEach((topic) => {
+            expect(topic).toHaveProperty("description", expect.any(String));
+            expect(topic).toHaveProperty("slug", expect.any(String));
+          });
+          expect(topics).toHaveLength(3);
+        });
+    });
+
+    test("GET 404: /api/topics returns a 404 message when there is an error in the request ", () => {
       return request(app)
         .get("/api/topicsa")
         .expect(404)
@@ -27,18 +43,28 @@ describe("app()", () => {
         });
     });
   });
-  test("GET 200: /api/topics should return an array of topic objects which has the slug and description properties ", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then((data) => {
-        const { body } = data;
-        const topics = body.topics;
-        topics.forEach((topic) => {
-          expect(topic).toHaveProperty("description", expect.any(String));
-          expect(topic).toHaveProperty("slug", expect.any(String));
+
+  describe("/api/", () => {
+    test("GET 200: /api/ should return a 200 status", () => {
+      return request(app).get("/api/").expect(200);
+    });
+
+    test("GET 200: /api/ should return an object describing all available endpoints of api", () => {
+      return request(app)
+        .get("/api/")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toEqual(apiList);
         });
-        expect(topics).toHaveLength(3);
-      });
+    });
+
+    test("GET 404: /api/ should return a 404 message when there is an error in the request ", () => {
+      return request(app)
+        .get("/apio/")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("page not found");
+        });
+    });
   });
 });
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const { getApi } = require("./controllers/api-controllers");
 const { getTopics } = require("./controllers/topic-controllers");
 
 app.use(express.json());
+app.get("/api/", getApi);
 app.get("/api/topics", getTopics);
 
 app.use((request, response, next) => {
