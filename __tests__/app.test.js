89df26345a6f9c632ca225b43ae58e000acec2aa
IanTibble1 <iantibble1@gmail.com>
2023-08-18 09:54:25 +0100

    adds GET 200: /api/articles/:article_id comment_count feature

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -546,15 +546,39 @@ describe("app()", () => {
           });
       });
     });
+    test("GET 200: should return a 200 status and produce appropirate response when multiple queries chained", () => {
+      return request(app)
+        .get("/api/articles?sort_by=author&order=asc")
+        .expect(200)
+        .then((data) => {
+          const { body } = data;
+          const articles = body.articles;
+          expect(articles).toBeSortedBy("author", { ascending: true });
+        });
+    });
   });
-  test("GET 200: should return a 200 status and produce appropirate response when multiple queries chained", () => {
-    return request(app)
-      .get("/api/articles?sort_by=author&order=asc")
-      .expect(200)
-      .then((data) => {
-        const { body } = data;
-        const articles = body.articles;
-        expect(articles).toBeSortedBy("author", { ascending: true });
-      });
+
+  describe("GET /api/articles/:article_id comment_count feature", () => {
+    test("GET 200: should return 200 status and add comment_count feature to :article_id endpoint ", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then((data) => {
+          const { body } = data;
+          const articles = body.articles;
+          expect(articles).toEqual({
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: "2020-07-09T20:11:00.000Z",
+            votes: 0,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+            comment_count: 11,
+          });
+        });
+    });
   });
 });
